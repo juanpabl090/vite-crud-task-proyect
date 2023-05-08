ccedf9f94cdb988f5fb77726bdf5cf3f74784c94
juanpabl090 <juanrona12@gmail.com>
2023-05-08 15:11:57 -0600

    Start up proyect

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <div>
+      <Navbar bg="dark" variant='dark' expand="lg">
+        <Container>
+          <Navbar.Brand>React MySQL</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to='/'>Home</Nav.Link>
+              <Nav.Link as={Link} to='/new'>New</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
---
+++ src/pages/TasksForm.jsx
@@ -0,0 +1,5 @@
+export default function TasksForm() {
+  return (
+    <div>TasksForm</div>
+  )
+}
